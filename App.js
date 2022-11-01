682f0737ea58c757dd710ebfda2adf322fa86b0f
Patrick <sawthedeveloper@gmail.com>
2022-11-01 16:25:05 -0700

    compile error: Babel module not found

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  {TailwindProvider} from "tailwindcss-react-native";
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./screens/HomeScreen"
 
